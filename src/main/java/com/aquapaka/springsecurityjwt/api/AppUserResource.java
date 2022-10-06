d21c96418031438675f1f6f8dc80a87c9496a478
aquapaka <aqua.tamlong@gmail.com>
2022-10-06 15:26:14 +0700

    Add save user and save role

--- src/main/java/com/aquapaka/springsecurityjwt/api/AppUserResource.java
+++ src/main/java/com/aquapaka/springsecurityjwt/api/AppUserResource.java
@@ -1,12 +1,11 @@
 package com.aquapaka.springsecurityjwt.api;
 
 import com.aquapaka.springsecurityjwt.model.AppUser;
+import com.aquapaka.springsecurityjwt.model.Role;
 import com.aquapaka.springsecurityjwt.service.AppUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +18,15 @@ public class AppUserResource {
     @GetMapping("/appUsers")
     public ResponseEntity<List<AppUser>> getAppUsers() {
         return ResponseEntity.ok().body(appUserService.getAppUsers());
+    }
+
+    @PostMapping("/appUser/save")
+    public ResponseEntity<AppUser> saveUser(@RequestBody AppUser appUser) {
+        return ResponseEntity.ok().body(appUserService.saveAppUser(appUser));
+    }
+
+    @PostMapping("/role/save")
+    public ResponseEntity<Role> saveRole(@RequestBody Role role) {
+        return ResponseEntity.ok().body(appUserService.saveRole(role));
     }
 }
